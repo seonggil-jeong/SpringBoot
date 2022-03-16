7265be60e57cfe53eea88151a246b710950aeeb1
JeongSeongGil <dataofsg02@gmail.com>
2022-03-16 20:49:24 +0900

    HETEOAS 적용

--- RESTful/src/main/java/com/example/restfulservice/user/UserController.java
+++ RESTful/src/main/java/com/example/restfulservice/user/UserController.java
@@ -1,13 +1,23 @@
 package com.example.restfulservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn; // static 으로 import 하여 사용 / WebMvcLinkBuilder.methodOn (X), methodOn (O)
 
 @RestController
 @Slf4j
@@ -26,13 +36,35 @@ public class UserController {
 
 
     @GetMapping("/users/{id}") // int 로 선언하여 int 값으로 매핑
-    public User retrieveOneUsers(@PathVariable int id) {
+    public MappingJacksonValue retrieveOneUsers(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id)); // 오류 반환
         }
-        return user;
+
+        // HATEOAS / 반환 값으로 사용되는 정보에 현재 상태(권한 등을 고려해서)에서 사용할 수 있는 또다른 링크의 주소를 같이 얻을 수 있음
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers()); // retrieveAllUser link 가져오기
+        model.add(linkTo.withRel("all-users")); // all-users : {url} 로 저장
+//        {
+//            "id": 1,
+//                "name": "Jeong",
+//                "_links": {
+//            "all-users": {
+//                "href": "http://localhost:8080/users"
+//            }
+//        }
+//        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(model);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
     // Headers 에 Content-Type : application/json 추가
