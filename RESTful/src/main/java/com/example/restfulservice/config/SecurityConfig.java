0e7f4db5682c86d86b9cf8213bf874e125d8342c
JeongSeongGil <dataofsg02@gmail.com>
2022-03-16 23:39:40 +0900

    H2

--- RESTful/src/main/java/com/example/restfulservice/config/SecurityConfig.java
+++ RESTful/src/main/java/com/example/restfulservice/config/SecurityConfig.java
@@ -3,10 +3,19 @@ package com.example.restfulservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers(("/h2-console/**")).permitAll(); // h2 ~ 로 오는 모든 값을 인증 없이 통과
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
