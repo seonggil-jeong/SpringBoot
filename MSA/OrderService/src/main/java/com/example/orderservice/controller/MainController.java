212eb882b3a392cf32a6f1013001225d7a977150
JeongSeonggil <dataofsg02@gmail.com>
2022-03-23 21:52:27 +0900

    OrderService

---
+++ MSA/OrderService/src/main/java/com/example/orderservice/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.example.orderservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/order-service")
+public class MainController {
+    private final Environment env;
+
+    @Autowired
+    public MainController(Environment env){ this.env = env; }
+
+    @GetMapping("/health")
+    public String status() {
+        return "(1)" + String.format("%s (%s)", env.getProperty("spring.application.name"), env.getProperty("local.server.port"));
+    }
+}
