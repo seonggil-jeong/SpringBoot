347c1247b6abed945e2014ab746e388b40727f32
JeongSeonggil <dataofsg02@gmail.com>
2022-03-31 18:44:53 +0900

    ConfigService

---
+++ MSA/ConfigService/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer // ConfigSever 명시
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
