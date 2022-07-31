2ee9b11311ea70b328aad5db4fa521ebf37c0c2a
JeongSeonggil <dataofsg02@gmail.com>
2022-07-31 23:14:47 +0900

    [TDD] Repository : MembershipRepositorySelect

--- RESTful/src/main/java/com/example/restfulservice/tdd/MembershipRepository.java
+++ RESTful/src/main/java/com/example/restfulservice/tdd/MembershipRepository.java
@@ -3,8 +3,12 @@ package com.example.restfulservice.tdd;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
+
+    List<Membership> findAllByUserId(final String userId);
 }
